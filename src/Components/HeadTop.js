afcd85d1340b194fbaf61bf07069ee7cc3cc3980
Your Name <sahanashetty151999@gmail.com>
2021-08-22 13:39:20 +0530

    Packages

--- src/Components/HeadTop.js
+++ src/Components/HeadTop.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import {Row,Container, Col} from "react-bootstrap";
+import React from "react";
+import { Row, Container, Col } from "react-bootstrap";
 import logo from "../../src/logo_web.png";
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch } from "react-icons/fa";
 function HeadTop(props) {
-    return (
-        <div className="app-header">
-           <Container>
-               <Row className="py-5">
-                   <Col md="8">
-                       <img className="logo" src={logo} alt="Weather Application"/>
-                   </Col>
-                   <Col md="4">
-                        <div className="search-box">
-                            
-                            <input placeholder="Search City" className="search-input" name="city"/>    
-                              
-<button className="search-btn" ><FaSearch/></button>
-                        </div>      
-                   </Col>
-                </Row>
-            </Container> 
-        </div>
-    );
+  return (
+    <div className="app-header">
+      <Container>
+        <Row className="py-5">
+          <Col md="8">
+            <img className="logo" src={logo} alt="Weather Application" />
+          </Col>
+          <Col md="4">
+            <div className="search-box">
+              <input
+                placeholder="Search City"
+                className="search-input"
+                name="city"
+              />
+              <button className="search-btn">
+                <FaSearch />
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default HeadTop;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import {Navbar,Container, Nav} from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 function Navi(props) {
-    let curDate = Date().toLocaleString()
+  let curDate = Date().toLocaleString();
 
-    return (
-        <div>
-            <Navbar>
-  <Container>
-  <Navbar id="responsive-navbar-nav">
-    <Nav className="nav-items">
-      <Nav.Link href="#Home">HOME</Nav.Link>
-      <Nav.Link href="#Favourite">FAVOURITE</Nav.Link> 
-      <Nav.Link href="#Recent">RECENT SEARCH</Nav.Link>
-    </Nav>
-  </Navbar>
-  <Nav>
-  <p className="date-time">
-              {curDate.slice(0,3)}, {curDate.slice(4,21)} Hours
+  return (
+    <div>
+      <Navbar>
+        <Container>
+          <Navbar id="responsive-navbar-nav">
+            <Nav className="nav-items">
+              <Nav.Link href="#Home">HOME</Nav.Link>
+              <Nav.Link href="#Favourite">FAVOURITE</Nav.Link>
+              <Nav.Link href="#Recent">RECENT SEARCH</Nav.Link>
+            </Nav>
+          </Navbar>
+          <Nav>
+            <p className="date-time">
+              {curDate.slice(0, 3)}, {curDate.slice(4, 21)} Hours
             </p>
-  </Nav>
-  </Container>
-</Navbar>
-        </div>
-    );
+          </Nav>
+        </Container>
+      </Navbar>
+      <hr />
+    </div>
+  );
 }
 
 export default Navi;
